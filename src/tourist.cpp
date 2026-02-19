e52ea514d6838fae13e612e62f85db1c4781b38a
HQMarius <mariusulrichvase@gmail.com>
2026-02-19 16:14:15 +0100

    123

--- src/tourist.cpp
+++ src/tourist.cpp
@@ -3,16 +3,19 @@
 // Implement this (visitor pattern)
 void Tourist::visit(Monkey &monkey)
 {
+    visited.push_back("monkey"); // implementeret med pushback
 }
 
 // Implement this (visitor pattern)
 void Tourist::visit(Tiger &tiger)
 {
+    visited.push_back("tiger");
 }
 
 // Implement this (visitor pattern)
-void Tourist::visit(Fish &monkey)
+void Tourist::visit(Fish &fish) // vi har også rettet &monkey til &fish
 {
+    visited.push_back("fish");
 }
 
 std::vector<std::string> Tourist::list_visited()
